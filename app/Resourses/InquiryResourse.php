afb124d77ca641e03242757da103110c8daaf244
et397 <b10812031@crepowermay.com>
2024-05-14 16:44:13 +0800

    增加獲得最新十筆諮詢紀錄的 Resourse

---
+++ app/Resourses/InquiryResourse.php
@@ -0,0 +1,22 @@
+<?php
+    namespace App\Http\Resources;
+    use Illuminate\Http\Resources\Json\JsonResource;
+
+    class InquiryResourse extends JsonResource
+    {
+        public function toArray($Inquirys)
+        {
+            return [
+                'company_name' => $Inquirys->company_name,
+                'contact_person' => $Inquirys->contact_person,
+                'contact_email' => $Inquirys->contact_email,
+                'contact_phone' => $Inquirys->contact_phone,
+                'company_location' => $Inquirys->company_location,
+                'business_registration' => $Inquirys->business_registration,
+                'industry_category'  => $Inquirys->industry_category,
+                'consultation_item' => $Inquirys->consultation_item,
+                'number_of_users' => $Inquirys->number_of_users,
+                'notes' => $Inquirys->notes,
+            ];
+        }
+    }
