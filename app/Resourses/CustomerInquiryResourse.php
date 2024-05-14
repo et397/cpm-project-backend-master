431235958eb67a6195d268c9970a26b1aab29608
et397 <b10812031@crepowermay.com>
2024-05-14 16:43:12 +0800

    新增初始選項的Resourse

---
+++ app/Resourses/CustomerInquiryResourse.php
@@ -0,0 +1,16 @@
+<?php
+    namespace App\Http\Resources;
+    use Illuminate\Http\Resources\Json\JsonResource;
+
+    class CustomerInquiryResource extends JsonResource
+    {
+        public function toArray($city = [], $industry = [], $inquiry = [], $is_business_registered = [])
+        {
+            return [
+                'city' => $city,
+                'industry' => $industry,
+                'inquiry' => $inquiry,
+                'is_business_registered' => $is_business_registered,
+            ];
+        }
+    }
