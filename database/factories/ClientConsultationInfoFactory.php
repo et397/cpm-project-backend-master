a4d9b8b5112ce558b888c466b8dc510773ed58d6
et397 <b10812031@crepowermay.com>
2024-05-14 11:10:52 +0800

    客戶資訊工廠consultation_item欄位 改從詢問項目model拿

--- database/factories/ClientConsultationInfoFactory.php
+++ database/factories/ClientConsultationInfoFactory.php
@@ -5,6 +5,7 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use App\Models\IsBusinessRegistered;
 use App\Models\CityDirectory;
+use App\Models\InquiryItems;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\ClientConsultationInfo>
@@ -26,14 +27,14 @@ class ClientConsultationInfoFactory extends Factory
             'contact_person' => $this->faker->name,
             'contact_email' => $this->faker->safeEmail,
             'contact_phone' => $this->faker->phoneNumber,
-            //到時從縣市model裡拿資料
+            //從縣市model裡拿資料
             'company_location' => $this->faker->randomElement(CityDirectory::directories()['city']),
-            //到時從營業登記表裡model裡拿資料
+            //從營業登記表裡model裡拿資料
             'business_registration' => $this->faker->randomElement(IsBusinessRegistered::description()['status']),
             //到時從產業類別model裡拿資料
             'industry_category' => $this->faker->word,
-            //到時從諮詢項目model裡拿資料
-            'consultation_item' => $this->faker->word,
+            //從諮詢項目model裡拿資料
+            'consultation_item' => $this->faker->randomElement(InquiryItems::description()['item']),
             'number_of_users' => ("consultation_item" == "報價") ? $this->faker->numberBetween(1, 100) : null,
             'notes' => $this->faker->sentence,
         ];
